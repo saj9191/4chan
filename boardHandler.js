9d15dd9dd32592ed2666fbbadad2f25de4a31360
Shannon Joyner <sjoyner@host-128-237-198-162.wv.cc.cmu.edu>
2012-10-04 21:31:14 -0400

    added boardHandler

---
+++ boardHandler.js
@@ -0,0 +1,62 @@
+var boardHandler = {
+	currentBoard: undefined,
+
+	boards: {
+		'Music': 'mu',
+		'Video Games': 'v'
+	},
+
+	boardThreads: {
+	    'mu' : {
+	        followedThreadIds : {},
+	        ignoredThreadIds : {},
+	        threadIds : {},
+	    },
+	    'v' : {
+	        followedThreadIds : {},
+	        ignoredThreadIds : {},
+	        threadIds: {},
+	    },
+	},
+
+	addThreadToSideBar: function(number) {
+    	var thread = $('<div/>');
+        thread.html('Thread '+ number);
+        thread.css('marginLeft', '10px');
+        thread.attr('id', number);
+        thread.css('font-size', 'small');
+        thread.css('padding-top', '10px');
+        // Prevents cursor from changing when you
+        // hover overs div
+        thread.css('cursor', 'default');
+        $('#recentThreads').append(thread);
+        thread.click(scraper.onThreadClick);
+    },
+
+    removeThreadsFromSideBar: function() {
+    	console.log('b', document.getElementById('recentThreads'));
+    	$('#recentThreads').empty();
+    	console.log('a', $('recentThreads'));
+    },
+
+	changeBoard: function() {
+		var selected = $('select option:selected')[0].text;
+		var folder = boardHandler.getBoardFolder(selected);
+		// Treat like no board selected
+		if (folder === undefined) {
+			return
+		} else {
+			scraper.removeThreadDiv();
+			boardHandler.removeThreadsFromSideBar();
+			boardHandler.currentBoard = folder;
+			scraper.followedThreadIds = boardHandler.boardThreads[folder].followedThreadIds;
+			scraper.ignoredThreadIds = boardHandler.boardThreads[folder].ignoredThreadIds;
+			scraper.threadIds = boardHandler.boardThreads[folder].threadIds;
+			scraper.getPage(1);
+		}
+	},
+
+	getBoardFolder: function(boardName) {
+		return boardHandler.boards[boardName];
+	}
+}
